4232ea1662307a144a44ee3a3ec4839fda54700f
Thomas Braam <thomas.braam@protonmail.com>
2021-02-03 22:34:14 +0100

    optimize imports

--- flink_processor/src/main/scala/org/twbraam/processor/UselessFunction.scala
+++ flink_processor/src/main/scala/org/twbraam/processor/UselessFunction.scala
@@ -1,11 +1,6 @@
 package org.twbraam.processor
 
-import com.google.protobuf.any.Any.messageCompanion
 import org.apache.flink.api.common.functions.MapFunction
-import io.circe._
-import scalapb_circe.JsonFormat
-
-import scala.util.matching.Regex
 
 class UselessFunction extends MapFunction[String, Float] {
   override def map(r: String): Float = {
