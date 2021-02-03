4c64ee3daa5e8d0259b5a26a78cb2bc852532f69
Thomas Braam <thomas.braam@protonmail.com>
2021-02-03 22:33:57 +0100

    wrote very bad regex for extracting data from source string

--- flink_processor/src/main/scala/org/twbraam/processor/UselessFunction.scala
+++ flink_processor/src/main/scala/org/twbraam/processor/UselessFunction.scala
@@ -5,11 +5,20 @@ import org.apache.flink.api.common.functions.MapFunction
 import io.circe._
 import scalapb_circe.JsonFormat
 
+import scala.util.matching.Regex
+
 class UselessFunction extends MapFunction[String, Float] {
   override def map(r: String): Float = {
+    val pattern = "^.*,title=(.+?),id=.*,link=(.+?),author=.*,date=(.+?)}$".r
+    val result = pattern.findFirstMatchIn(r) match {
+      case Some(found) => found.subgroups match {
+        case List(title, link, timestamp) => Some(title, link, timestamp)
+        case _ => None
+      }
+      case _ => None
+    }
 
-    val json = JsonFormat.fromJsonString(r)
-    print(json)
+    print(result)
 
     0.001f
   }
